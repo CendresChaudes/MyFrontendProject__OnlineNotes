7b76d36ca9024aa79dcc5203cac01684309e0b4b
CendresChaudes <romqaaa1337@gmail.com>
2023-10-25 14:11:41 +0300

    refactor: рефакторит стили

--- src/app/styles/theme.config.ts
+++ src/app/styles/theme.config.ts
@@ -10,7 +10,7 @@ export const theme = {
       headerBg: '#00b96b',
       colorBorderSecondary: '#00b96b',
       colorFillAlter: '#00b96b',
-      lineWidth: 2,
+      lineWidth: 3,
       boxShadowCard: '0 1px 5px 2px rgba(0, 185, 107, 0.16), 0 3px 6px 2px rgba(0, 185, 107, 0.12), 0 5px 12px 4px rgba(0, 185, 107, 0.09)',
       boxShadowTertiary: '0 1px 2px 0 rgba(0, 185, 107, 0.03), 0 1px 6px -1px rgba(0, 185, 107, 0.02), 0 2px 4px 0 rgba(0, 185, 107, 0.02)'
     }
