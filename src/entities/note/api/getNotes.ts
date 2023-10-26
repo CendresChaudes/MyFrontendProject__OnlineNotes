bd87ceb224f29d00c1fa25a3e960838e8de97565
CendresChaudes <romqaaa1337@gmail.com>
2023-10-26 20:03:33 +0300

    feat: добавляет нотификацию для кейса загрузки карточек

--- src/entities/note/api/getNotes.ts
+++ src/entities/note/api/getNotes.ts
@@ -1,13 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, runTransaction } from 'firebase/firestore';
+import { changeNotification } from '@/shared/lib';
 import { APIRoute } from '@/const';
 
 export const getNotes = createAsyncThunk<
   INoteData[],
   undefined,
   FirebaseThunkAPI
->('api/getNotes', async (_, { extra: api }) => {
-  const response = await getDocs(collection(api, APIRoute.Notes));
+>(
+  'api/getNotes',
+  async (_, { dispatch, extra: api }) => {
+    try {
+      const docsRef = collection(api, APIRoute.Notes);
+      let data: unknown;
 
-  return response.docs.map((item) => item.data() as INoteData);
-});
+      await runTransaction(api, async () => {
+        const response = await getDocs(docsRef);
+        data = response.docs.map((item) => item.data() as INoteData);
+      });
+
+      return data as INoteData[];
+    } catch {
+      dispatch(
+        changeNotification({
+          type: 'error',
+          title: 'Ошибка!',
+          text: 'Не удалось загрузить заметки',
+        })
+      );
+
+      throw new Error();
+    }
+  });
