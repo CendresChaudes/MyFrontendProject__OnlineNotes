19d897b0cb8c8c85dc413b15da2fc830b69d9bcd
CendresChaudes <romqaaa1337@gmail.com>
2023-10-26 18:18:36 +0300

    feat: добавляет нотификацию для неудачного добавления заметки

--- src/entities/note/api/postNote.ts
+++ src/entities/note/api/postNote.ts
@@ -1,18 +1,35 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, runTransaction } from 'firebase/firestore';
+import { changeNotification } from '@/shared/lib';
 import { APIRoute } from '@/const';
 
 export const postNote = createAsyncThunk<
   INoteData,
   [INoteData, () => void],
   FirebaseThunkAPI
-    >
-    ('api/postNote', async ([newNoteData, callback], { extra: api }) => {
-      await setDoc(doc(api, APIRoute.Notes, newNoteData.id), newNoteData);
+    >('api/postNote', async ([newNoteData, callback], { dispatch, extra: api }) => {
+      const docRef = doc(api, APIRoute.Notes, newNoteData.id!);
+      delete newNoteData['id'];
+      try {
+        // eslint-disable-next-line @typescript-eslint/require-await
+        await runTransaction(api, async (transaction) => {
+          transaction.set(docRef, newNoteData);
+        });
 
-      if (callback) {
-        callback();
+        if (callback) {
+          callback();
+        }
+
+        return newNoteData;
+      } catch {
+        dispatch(
+          changeNotification({
+            type: 'error',
+            title: 'Ошибка!',
+            text: 'Не удалось добавить новую заметку',
+          })
+        );
+
+        throw new Error();
       }
-
-      return newNoteData;
     });
